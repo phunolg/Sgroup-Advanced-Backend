87f7b44a4f428df4b51b5818ad6124c4bb9a30e7
Phuong Tran <phuong0968804740@gmail.com>
2025-09-22 00:12:19 +0700

    test husky, lint-staged

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {Controller, Get } from '@nestjs/common';
+import { AppService} from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService
+    
+    ) {
+
+  }
 
   @Get()
   getHello(): string {
+    const a = 1;
     return this.appService.getHello();
   }
 
   @Get('name')
   getName(): string {
-    return this.appService.getName();
+
+    
+    return this.appService.getName()
+    ;
   }
 
   @Get('age')
   getAge(): number {
+
+
     return this.appService.getAge();
   }
 }
